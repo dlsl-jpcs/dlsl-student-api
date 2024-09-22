032c91f7427bb27c14adcf5cd8b88a4585a7ce43
Tyron <lucena.tyronscott@gmail.com>
2024-09-23 00:38:55 +0800

    initial commit

---
+++ index.ts
@@ -0,0 +1,74 @@
+
+// proxy for getting dlsl student information
+const server = Bun.serve({
+    async fetch(request, server) {
+        console.log("Request", request.url);
+        const url = new URL(request.url);
+        if (url.pathname === "/api/student") {
+            const id = url.searchParams.get("id");
+            if (!id) {
+                return new Response("Missing id", {
+                    status: 400
+                });
+            }
+
+            const student = await getStudentInfo(id);
+            if (isValid(student)) {
+                return new Response(JSON.stringify(student), {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*", // Enable CORS
+                    }
+                });
+            } else {
+                return new Response("Student not found", {
+                    status: 404
+                });
+            }
+        }
+
+        return new Response("Not found", {
+            status: 404
+        });
+    },
+});
+
+function isValid(object: any) {
+    if (!object) return false;
+
+    const email = object.email_address;
+    if (!email) return false;
+
+    // empty email?
+    if (email.length === 0) return false;
+
+    return true;
+}
+
+/**
+ * Uses the DLSL tap register API to get the student's email and department
+ * 
+ * TODO: is this legal? :o
+ * @param id student id
+ */
+export async function getStudentInfo(id: string): Promise<{ email_address: string, department: string }> {
+
+    const regKey = Bun.env["REG_KEY"]!;
+    const api = "https://sandbox.dlsl.edu.ph/registration/event/helper.php";
+
+    const response = await fetch(api, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            action: "registration_tapregister",
+            regkey: regKey,
+            card_tag: id,
+        }),
+    });
+
+    return await response.json();
+}
+
+console.log("Server started at", server.url.host, "Port", server.port);
